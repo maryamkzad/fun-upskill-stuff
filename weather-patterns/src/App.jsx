9ab1b5eee21b9d69a1a57d4bcd366ed6bdf006b4
“Maryam <mz107@wellesley.edu>
2025-01-14 11:38:10 -0500

    modulariz the weather patterns

--- weather-patterns/src/App.jsx
+++ weather-patterns/src/App.jsx
@@ -11,26 +11,16 @@ function App() {
       <h1>Weather Patterns</h1>
       <div>
         <p>We love weather. Here are some of the weather patterns that have been intresting lately.</p>
-        </div> 
         <h3>Snow</h3>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <ul>
+          <li>All snowflakes are diffrent</li>
+          <li>Its cold</li>
+          <li>It reflects light (and can give you a sunburn)</li>
+        </ul>
+        </div> 
+
+
+       
     </>
   )
 }
